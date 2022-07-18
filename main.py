1393b4ef38a643f282fae95ef995a375729f5531
Luke Irwin <89871736+lukeirwin03@users.noreply.github.com>
2022-07-18 11:48:06 -0500

    Added FIles

---
+++ main.py
@@ -0,0 +1,74 @@
+import tweepy
+import configparser
+import textGenerationHappyTransformer as textGen
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+FILE = 'id.txt'
+
+# getting the information from the config.ini file
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+user_ID = config['twitter']['user_ID']
+
+def authenticate():
+    '''Authenticates the informations and creates the api object
+
+    :rtype: object
+    :return: tweepy api object
+    '''
+    auth = tweepy.OAuthHandler(api_key,api_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    return api
+
+def retrieve_id(file):
+    '''Gets the tweet id of the last tweet that has been replied to
+
+    :param file: the name of the file has the last seen tweet's ID written to it
+    :type file: string
+    :rtype: int
+    :return: the tweet id of the last tweet that has been replied to
+    '''
+    reader = open(file, 'r')
+    last_seen_id = int(reader.read().strip())
+    reader.close()
+    return last_seen_id
+
+def store_id(id, file):
+    '''Stores the id of the last seen tweet to the file
+
+    :param id: the id of the tweet that the bot is replying to
+    :type id: int
+    :param file: the name of the file has the last seen tweet's ID written to it
+    :type file: string 
+    '''
+    writer = open(file, 'w')
+    writer.write(str(id))
+    writer.close()
+
+def reply(api):
+    '''Uses the api object to reply to any tweets if there are any.
+
+    :param api: tweepy api object
+    :type api: object
+    '''
+    last_seen_id = retrieve_id(FILE)
+    mentions = api.mentions_timeline(since_id=last_seen_id)
+
+    for mention in reversed(mentions):
+        if 'HeyDexter' in mention.text:
+            message = (mention.text.split('HeyDexter', 1)[1])
+            last_seen_id = mention.id
+            store_id(last_seen_id, FILE)
+            api.update_status(status=('@' + mention.user.screen_name + ' ' + textGen.dialoGPT(message)), auto_populate_reply_metadata=True)
+            print('Replied to @'+mention.user.screen_name)
+
+def main():
+    api = authenticate()
+    reply(api)
+
+if __name__ == '__main__':
+    main()
---
+++ textGenerationHappyTransformer.py
@@ -0,0 +1,79 @@
+from unittest import result
+from happytransformer import HappyGeneration, GENSettings
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+def neoGPT(prompt):
+    '''Method to generate and return a response to a prompt that is passed into the method using the GPT Neo Model.
+    
+    :param prompt: the prompt that the model generates text from.
+    :type prompt: string
+    :rtype: string
+    :return: generated text based on the prompt given
+    '''
+    top_p_sampling_settings = GENSettings(do_sample=True, top_k=0, top_p=0.8, temperature=0.7, no_repeat_ngram_size=2)
+    happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-125M")
+    result = happy_gen.generate_text(prompt, args=top_p_sampling_settings)
+    return result.text
+
+def dialoGPT(prompt):
+    '''Method to generate and return a response to a prompt that is passed into the method using the GPT Dialo Model.
+
+    :param prompt: the prompt that the model generates text from.
+    :type prompt: string
+    :rtype: string
+    :return: generated text based on the prompt given
+    '''
+
+    modelName = 'microsoft/DialoGPT-medium'
+    tokenizer = AutoTokenizer.from_pretrained(modelName)
+    model = AutoModelForCausalLM.from_pretrained(modelName)
+
+    # encode the input and add end of string token
+    input_ids = tokenizer.encode(prompt + tokenizer.eos_token, return_tensors="pt")
+    # generate a bot response
+    chat_history_ids = model.generate(
+        input_ids,
+        max_length=1000,
+        do_sample=True,
+        top_k=100,
+        temperature=0.85,
+        pad_token_id=tokenizer.eos_token_id
+    )
+    #print the output
+    output = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+    return output
+
+if __name__ == '__main__':
+    message = 'What is the meaning of life?'
+
+    print(f'GPT Neo Model: {neoGPT(message)}')
+    print()
+    print(f'GPT Dialo Model: {dialoGPT(message)}')
+    
+
+# for later use 
+
+# def dialoGPT():
+#     modelName = 'microsoft/DialoGPT-medium'
+#     tokenizer = AutoTokenizer.from_pretrained(modelName)
+#     model = AutoModelForCausalLM.from_pretrained(modelName)
+#     for step in range(5):
+#         # take user input
+#         text = input(">> You:")
+#         # encode the input and add end of string token
+#         input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
+#         # concatenate new user input with chat history (if there is)
+#         bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
+#         # generate a bot response
+#         chat_history_ids = model.generate(
+#             bot_input_ids,
+#             max_length=1000,
+#             do_sample=True,
+#             top_k=100,
+#             temperature=0.75,
+#             pad_token_id=tokenizer.eos_token_id
+#         )
+#         #print the output
+#         output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+#         print(f"DialoGPT: {output}")
